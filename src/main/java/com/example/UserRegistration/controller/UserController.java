91ac1a4f6f13b7b01ebd33e816df0b79f2c7d30a
aachal-vaishnav <160655330+aachal-vaishnav@users.noreply.github.com>
2026-01-02 19:03:05 +0530

    Add Validations to email and password

--- src/main/java/com/example/UserRegistration/controller/UserController.java
+++ src/main/java/com/example/UserRegistration/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.UserRegistration.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,16 @@ public class UserController {
     UserService userService;
 
     @RequestMapping("/")
-    public String showRegistrationForm(){
-        return "user";
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user",new User());
+        return "userRegister";
     }
 
     @RequestMapping("/register")
     public String registerUser(@ModelAttribute @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "userRegister";
+        }
         userService.saveUser(user);
         return "result";
     }
--- src/main/java/com/example/UserRegistration/entity/User.java
+++ src/main/java/com/example/UserRegistration/entity/User.java
@@ -1,24 +1,36 @@
 package com.example.UserRegistration.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name="users")
 public class User {
 
+
+
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "username required")
-    @Size(min = 6,max = 12,message = "username between must be 6-12")
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 12, message = "Username must be between 6 and 12 characters")
     private String username;
 
-
+    @NotNull(message = "Email is required")
+    @Email(message = "Enter a valid email address")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    @Pattern.List(value = {
+    @Pattern(regexp=".*[A-Z].*",message = "Password should contain at least 1 Uppercase character"),
+    @Pattern(regexp=".*[a-z].*",message = "Password should contain at least 1 Lowercase character"),
+    @Pattern(regexp=".*\\d.*",message = "Password should contain at least 1 Number")
+    })
     private String password;
+
 
     public Long getId() {
         return id;
